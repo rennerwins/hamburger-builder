f0457a6f17d3c1bfd09c339c344b1cec9e7dc6dd
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-11-17 22:11:40 -0800

    Bootstrap Burger and BurgerIngredient together

---
+++ src/components/Burger/Burger.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
+
+const BurgerWrapper = styled.div`
+  width: 100%;
+  margin: auto;
+  height: 250px;
+  overflow: scroll;
+  text-align: center;
+  font-weight: bold;
+  font-size: 1.2rem;
+
+  @media (min-width: 1000px) and (min-height: 700px) {
+    width: 700px;
+    height: 600px;
+  }
+
+  @media (min-width: 500px) and (min-height: 401px) {
+    width: 450px;
+    height: 400px;
+  }
+
+  @media (min-width: 500px) and (min-height: 400px) {
+    width: 350px;
+    height: 300px;
+  }
+`;
+
+const burger = props => {
+  return (
+    <BurgerWrapper>
+      <BurgerIngredient type="bread-top" />
+      <BurgerIngredient type="cheese" />
+      <BurgerIngredient type="meat" />
+      <BurgerIngredient type="bread-bottom" />
+    </BurgerWrapper>
+  );
+};
+
+export default burger;
