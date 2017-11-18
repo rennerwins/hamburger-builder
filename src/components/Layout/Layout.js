be84b590eaa59576c09474ed1ea8bd5264cd0325
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-11-17 21:51:20 -0800

    Add styled-components

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
+import styled from 'styled-components';
 
 import Wrapper from '../../hoc/Wrapper';
+
+const MainContent = styled.main`margin-top: 16px;`;
 
 const layout = ({ children }) => (
   <Wrapper>
     <div>Toolbar, SideDrawer, Backdrop</div>
-    <main>{children}</main>
+    <MainContent>{children}</MainContent>
   </Wrapper>
 );
 
