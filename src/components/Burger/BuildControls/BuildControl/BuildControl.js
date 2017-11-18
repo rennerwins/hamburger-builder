83b080571e3110923cdc646c93646058775b6842
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-11-17 22:45:44 -0800

    Add BuildControls component

---
+++ src/components/Burger/BuildControls/BuildControl/BuildControl.js
@@ -0,0 +1,71 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const BuildControl = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 5px 0;
+`;
+
+const Button = styled.button`
+  display: block;
+  font: inherit;
+  padding: 5px;
+  margin: 0 5px;
+  width: 80px;
+  border: 1px solid #aa6817;
+  cursor: pointer;
+  outline: none;
+
+  &:disabled {
+    background-color: #ac9980;
+    border: 1px solid #7e7365;
+    color: #ccc;
+    cursor: default;
+  }
+
+  &:hover:disabled {
+    background-color: #ac9980;
+    color: #ccc;
+    cursor: not-allowed;
+  }
+`;
+
+const Label = styled.div`
+  padding: 10px;
+  font-weight: bold;
+  width: 80px;
+`;
+
+const Less = Button.extend`
+  background-color: #d39952;
+  color: white;
+
+  &:hover,
+  &:active {
+    background-color: #daa972;
+    color: white;
+  }
+`;
+
+const More = Button.extend`
+  background-color: #8f5e1e;
+  color: white;
+
+  &:hover,
+  &:active {
+    background-color: #99703f;
+    color: white;
+  }
+`;
+
+const buildControl = props => (
+  <BuildControl>
+    <Label>{props.label}</Label>
+    <Less>Less</Less>
+    <More>More</More>
+  </BuildControl>
+);
+
+export default buildControl;
---
+++ src/components/Burger/BuildControls/BuildControls.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import BuildControl from './BuildControl/BuildControl';
+
+const BuildControls = styled.div`
+  width: 100%;
+  background-color: #cf8f2e;
+  display: flex;
+  flex-flow: column;
+  align-items: center;
+  box-shadow: 0 2px 1px #ccc;
+  margin: auto;
+  padding: 10px 0;
+`;
+
+const controls = [
+  { label: 'Salad', type: 'salad' },
+  { label: 'Bacon', type: 'bacon' },
+  { label: 'Cheese', type: 'cheese' },
+  { label: 'Meat', type: 'meat' }
+];
+
+const builControls = props => (
+  <BuildControls>
+    {controls.map(ctrl => <BuildControl key={ctrl.label} label={ctrl.label} />)}
+  </BuildControls>
+);
+
+export default builControls;
