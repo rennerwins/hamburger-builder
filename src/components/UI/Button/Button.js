e170ab52fe1186ca3a3aff041ba00ffbf6eaa0d1
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-11-20 23:19:51 -0800

    Add dynamic styles to button

---
+++ src/components/UI/Button/Button.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background-color: transparent;
+  border: none;
+  outline: none;
+  cursor: pointer;
+  font: inherit;
+  padding: 10px;
+  margin: 10px;
+  font-weight: bold;
+  color: ${props => props.btnType === 'Success' && '#5c9210'};
+  color: ${props => props.btnType === 'Danger' && '#944317'};
+
+  &:first-of-type {
+    margin-left: 0;
+    padding-left: 0;
+  }
+`;
+
+const button = props => (
+  <Button btnType={props.btnType} onClick={props.clicked}>
+    {props.children}
+  </Button>
+);
+
+export default button;
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -87,6 +87,10 @@ class BurgerBuilder extends Component {
     this.setState(() => ({ purchasing: false }));
   };
 
+  purchaseContinueHandler = () => {
+    alert('You continue');
+  };
+
   render() {
     const disabledInfo = {
       ...this.state.ingredients
@@ -99,7 +103,12 @@ class BurgerBuilder extends Component {
     return (
       <Wrapper>
         <Modal show={this.state.purchasing} modalClosed={this.purchaseCancelHandler}>
-          <OrderSummary ingredients={this.state.ingredients} />
+          <OrderSummary
+            ingredients={this.state.ingredients}
+            price={this.state.totalPrice}
+            purchaseCancelled={this.purchaseCancelHandler}
+            purchaseContinued={this.purchaseContinueHandler}
+          />
         </Modal>
         <Burger ingredients={this.state.ingredients} />
         <BuildControls
