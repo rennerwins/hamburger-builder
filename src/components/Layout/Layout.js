ff4004cc38f18340d1f5fd69ac002a6ff24bf0e1
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-11-21 10:23:54 -0800

    Add side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react';
 
 import Wrapper from '../../hoc/Wrapper';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = ({ children }) => (
   <Wrapper>
     <Toolbar />
+    <SideDrawer />
     <main style={{ marginTop: '72px' }}>{children}</main>
   </Wrapper>
 );
---
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import styled from 'styled-components';
+
+import Logo from '../../Logo/Logo';
+import NavigationItems from '../NavigationItems/NavigationItems';
+
+const SideDrawer = styled.div`
+  position: fixed;
+  width: 280px;
+  max-width: 70%;
+  height: 100%;
+  left: 0;
+  top: 0;
+  z-index: 200;
+  background-color: white;
+  padding: 32px 16px;
+  box-sizing: border-box;
+  transition: transform 0.3s ease-out;
+
+  @media (min-width: 500px) {
+    display: none;
+  }
+`;
+
+const sideDrawer = props => {
+  return (
+    <SideDrawer>
+      <Logo />
+      <nav>
+        <NavigationItems />
+      </nav>
+    </SideDrawer>
+  );
+};
+
+export default sideDrawer;
