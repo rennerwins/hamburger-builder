a54e0ec417cea56bac330489ce51524b1a09d9a8
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-11-28 23:20:55 -0800

    Install axios and add firebase url

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,17 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from 'react'
 
-import Wrapper from '../../hoc/Wrapper/Wrapper';
-import Burger from '../../components/Burger/Burger';
-import BuildControls from '../../components/Burger/BuildControls/BuildControls';
-import Modal from '../../components/UI/Modal/Modal';
-import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary';
+import Wrapper from '../../hoc/Wrapper/Wrapper'
+import Burger from '../../components/Burger/Burger'
+import BuildControls from '../../components/Burger/BuildControls/BuildControls'
+import Modal from '../../components/UI/Modal/Modal'
+import OrderSummary from '../../components/Burger/OrderSummary/OrderSummary'
+import axios from '../../axios-orders'
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
   cheese: 0.4,
   meat: 1.3,
   bacon: 0.7
-};
+}
 
 class BurgerBuilder extends Component {
   state = {
@@ -24,80 +25,100 @@ class BurgerBuilder extends Component {
     totalPrice: 4,
     purchasable: false,
     purchasing: false
-  };
+  }
 
   updatePurchaseState = ingredients => {
     const sum = Object.keys(ingredients)
       .map(igKey => ingredients[igKey])
-      .reduce((sum, el) => sum + el, 0);
+      .reduce((sum, el) => sum + el, 0)
 
     this.setState(() => ({
       purchasable: sum > 0
-    }));
-  };
+    }))
+  }
 
   addIngredientHandler = type => {
-    const oldCount = this.state.ingredients[type];
-    const updatedCounted = oldCount + 1;
+    const oldCount = this.state.ingredients[type]
+    const updatedCounted = oldCount + 1
     const updatedIngredients = {
       ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCounted;
+    }
+    updatedIngredients[type] = updatedCounted
 
-    const priceAddition = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice + priceAddition;
+    const priceAddition = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice + priceAddition
 
     this.setState(() => ({
       totalPrice: newPrice,
       ingredients: updatedIngredients
-    }));
-    this.updatePurchaseState(updatedIngredients);
-  };
+    }))
+    this.updatePurchaseState(updatedIngredients)
+  }
 
   removeIngredientHandler = type => {
-    const oldCount = this.state.ingredients[type];
+    const oldCount = this.state.ingredients[type]
 
     if (oldCount <= 0) {
-      return;
+      return
     }
 
-    const updatedCounted = oldCount - 1;
+    const updatedCounted = oldCount - 1
     const updatedIngredients = {
       ...this.state.ingredients
-    };
-    updatedIngredients[type] = updatedCounted;
+    }
+    updatedIngredients[type] = updatedCounted
 
-    const priceDeduction = INGREDIENT_PRICES[type];
-    const oldPrice = this.state.totalPrice;
-    const newPrice = oldPrice - priceDeduction;
+    const priceDeduction = INGREDIENT_PRICES[type]
+    const oldPrice = this.state.totalPrice
+    const newPrice = oldPrice - priceDeduction
 
     this.setState(() => ({
       totalPrice: newPrice,
       ingredients: updatedIngredients
-    }));
-    this.updatePurchaseState(updatedIngredients);
-  };
+    }))
+    this.updatePurchaseState(updatedIngredients)
+  }
 
   purchaseHandler = () => {
-    this.setState(() => ({ purchasing: true }));
-  };
+    this.setState(() => ({ purchasing: true }))
+  }
 
   purchaseCancelHandler = () => {
-    this.setState(() => ({ purchasing: false }));
-  };
+    this.setState(() => ({ purchasing: false }))
+  }
 
-  purchaseContinueHandler = () => {
-    alert('You continue');
-  };
+  purchaseContinueHandler = async () => {
+    // alert('You continue');
+    const order = {
+      ingredients: this.state.ingredients,
+      price: this.state.totalPrice,
+      customer: {
+        name: 'Win Eiwwongcharoen',
+        address: {
+          street: 'Test 1',
+          zipCode: '12345',
+          country: 'USA'
+        },
+        email: 'test@test.com'
+      },
+      deliveryMethod: 'fastest'
+    }
+    try {
+      const res = await axios.post('/orders.json', order)
+      console.log(res)
+    } catch (err) {
+      console.log(err)
+    }
+  }
 
   render() {
     const disabledInfo = {
       ...this.state.ingredients
-    };
+    }
 
     for (let key in disabledInfo) {
-      disabledInfo[key] = disabledInfo[key] <= 0;
+      disabledInfo[key] = disabledInfo[key] <= 0
     }
 
     return (
@@ -120,8 +141,8 @@ class BurgerBuilder extends Component {
           ordered={this.purchaseHandler}
         />
       </Wrapper>
-    );
+    )
   }
 }
 
-export default BurgerBuilder;
+export default BurgerBuilder
