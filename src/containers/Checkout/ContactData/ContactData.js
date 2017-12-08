7681145047e28d8f167a3841d9eb2c2c998c5408
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-08 15:26:21 -0800

    Add a little bit of style to contact data form

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -7,6 +7,10 @@ const ContactDataWrapper = styled.div`
   margin: 20px auto;
   width: 80%;
   text-align: center;
+  box-shadow: 0 2px 3px #ccc;
+  border: 1px solid #eee;
+  padding: 10px;
+  box-sizing: border-box;
 
   @media (min-width: 600px) {
     width: 500px;
