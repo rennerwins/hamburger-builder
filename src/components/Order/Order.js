a9795c92da1c2fb8f89e0d6e6da8da51403f41c0
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-08 22:47:00 -0800

    Adding an order page

---
+++ src/components/Order/Order.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const OrderWrapper = styled.div`
+  width: 100%;
+  border: 1px solid #eee;
+  box-shadow: 0 2px 3px #ccc;
+  padding: 10px;
+  margin: 10px auto;
+  box-sizing: border-box;
+`;
+
+const order = props => (
+  <OrderWrapper>
+    <p>Ingredients: Salad (1)</p>
+    <p>
+      Price: <strong>USD 5.45</strong>
+    </p>
+  </OrderWrapper>
+);
+
+export default order;
