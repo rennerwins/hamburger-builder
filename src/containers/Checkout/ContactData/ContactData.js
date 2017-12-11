4fb137d822bba48bbc1e7e0fc6c30473648b2504
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-11 10:07:24 -0800

    Fix rules of undefined

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -107,16 +107,18 @@ class ContactData extends Component {
   checkValidity = (value, rules) => {
     let isValid = true;
 
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
+    if (rules) {
+      if (rules.required) {
+        isValid = value.trim() !== '' && isValid;
+      }
 
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
+      if (rules.minLength) {
+        isValid = value.length >= rules.minLength && isValid;
+      }
 
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
+      if (rules.maxLength) {
+        isValid = value.length <= rules.maxLength && isValid;
+      }
     }
 
     return isValid;
