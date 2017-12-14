0b14ebf4bd889eab6c3be606e1bb2fb059c277f4
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-14 10:39:03 -0800

    Setup redux in index.js

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import { BrowserRouter } from 'react-router-dom';
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
 
 import App from './App';
+import reducer from './store/reducer';
 import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
+const store = createStore(reducer);
+
 const app = (
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>
+  <Provider store={store}>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </Provider>
 );
 
 ReactDOM.render(app, document.getElementById('root'));
