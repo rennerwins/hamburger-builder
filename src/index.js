36090b5c11a03c92c27fa38710a197d25e6e1556
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-19 12:00:27 -0800

    Install redux-thunk

--- src/index.js
+++ src/index.js
@@ -1,8 +1,9 @@
 import { BrowserRouter } from 'react-router-dom';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { Provider } from 'react-redux';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import thunk from 'redux-thunk';
 
 import App from './App';
 import reducer from './store/reducers/burgerBuilder';
@@ -10,10 +11,9 @@ import registerServiceWorker from './registerServiceWorker';
 
 import './index.css';
 
-const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-);
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
+
+const store = createStore(reducer, composeEnhancers(applyMiddleware(thunk)));
 
 const app = (
   <Provider store={store}>
