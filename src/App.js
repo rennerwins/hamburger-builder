b60a5e3c285ac97127ad5cd03f7b8bde9a832135
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2017-12-28 10:18:01 -0800

    Adding logout link

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Auth from './containers/Auth/Auth';
 import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
 import Checkout from './containers/Checkout/Checkout';
 import Layout from './hoc/Layout/Layout';
+import Logout from './containers/Auth/Logout/Logout';
 import Orders from './containers/Orders/Orders';
 
 class App extends Component {
@@ -16,6 +17,7 @@ class App extends Component {
             <Route path="/checkout" component={Checkout} />
             <Route path="/orders" component={Orders} />
             <Route path="/auth" component={Auth} />
+            <Route path="/logout" component={Logout} />
             <Route path="/" exact component={BurgerBuilder} />
           </Switch>
         </Layout>
