ed0febf98ee2556a6bcbe1c7f7b515f6ca4e6fd1
Win Eiwwongcharoen <win.eiwwongcharoen@gmail.com>
2018-01-06 19:03:13 +0700

    Sharing validation method

--- src/containers/Auth/Auth.js
+++ src/containers/Auth/Auth.js
@@ -3,6 +3,7 @@ import { Redirect } from 'react-router-dom';
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import { checkValidity } from '../../shared/utility';
 import * as actions from '../../store/actions/index';
 import Button from '../../components/UI/Button/Button';
 import Input from '../../components/UI/Input/Input';
@@ -65,24 +66,6 @@ class Auth extends Component {
     }
   };
 
-  checkValidity = (value, rules) => {
-    let isValid = true;
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    return isValid;
-  };
-
   inputChangedHandler = (e, controlName) => {
     const { value } = e.target;
 
@@ -92,7 +75,7 @@ class Auth extends Component {
         [controlName]: {
           ...controls[controlName],
           value,
-          valid: this.checkValidity(value, controls[controlName].validation),
+          valid: checkValidity(value, controls[controlName].validation),
           touched: true
         }
       }
--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -2,6 +2,7 @@ import { connect } from 'react-redux';
 import React, { Component } from 'react';
 import styled from 'styled-components';
 
+import { checkValidity } from '../../../shared/utility';
 import * as actions from '../../../store/actions/index';
 import axios from '../../../axios-orders';
 import Button from '../../../components/UI/Button/Button';
@@ -109,24 +110,6 @@ class ContactData extends Component {
     formIsValid: false
   };
 
-  checkValidity = (value, rules) => {
-    let isValid = true;
-
-    if (rules.required) {
-      isValid = value.trim() !== '' && isValid;
-    }
-
-    if (rules.minLength) {
-      isValid = value.length >= rules.minLength && isValid;
-    }
-
-    if (rules.maxLength) {
-      isValid = value.length <= rules.maxLength && isValid;
-    }
-
-    return isValid;
-  };
-
   orderHandler = async e => {
     e.preventDefault();
     const { orderForm } = this.state;
@@ -164,7 +147,7 @@ class ContactData extends Component {
         [inputIdentifier]: {
           ...orderForm[inputIdentifier],
           value,
-          valid: this.checkValidity(value, orderForm[inputIdentifier].validation),
+          valid: checkValidity(value, orderForm[inputIdentifier].validation),
           touched: true
         }
       }
